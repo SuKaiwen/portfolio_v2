1a3926c5a00296ae7d45d453c871e2cd4bce1217
Kevin Su <kevinsu@outlook.co.nz>
2022-01-17 17:25:31 +1100

    about me redesign and exp redesign and responsiveness

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../css/About.css';
+
 import me from '../images/me.jpg';
+import mountain from '../images/mountain.jpg';
 
 function About(props) {
     return (
@@ -20,29 +22,35 @@ function About(props) {
                 <div className = "sub-container">
                     <div className = "main-card">
                         <div className= "row">
-                            <h1 className = "green-color">SUMMARY</h1>
-                            <h2>Core skills</h2>
-                            <div className = "skill-grid">
-                                <i class="fab fa-react fa-5x"></i>
-                                <i class="fab fa-js fa-5x"></i>
-                                <i class="fab fa-html5 fa-5x"></i>
-                                <i class="fab fa-css3-alt fa-5x"></i>
-                                <i class="fab fa-sass fa-5x"></i>
-                                <i class="fas fa-wind fa-5x"></i>
-                                <i class="fab fa-bootstrap fa-5x"></i>
-                                <i class="fab fa-java fa-5x"></i>
-                                <i class="fab fa-python fa-5x"></i>
+                            <img src = {mountain} alt = "sydney-banner" />
+                            <div className = "text">
+                                <h1>Core skills</h1>
+                                <p>My main area of expertise is front-end development. Here is a list of skills and stuff I can do:</p>
                             </div>
+                            <ul>
+                                <li><i class="fab fa-react fa-3x"></i><p>React</p></li>
+                                <li><i class="fab fa-js fa-3x"></i><p>JavaScript</p></li>
+                                <li><i class="fab fa-html5 fa-3x"></i><p>HTML</p></li>
+                                <li><i class="fab fa-css3-alt fa-3x"></i><p>CSS</p></li>
+                                <li><i class="fab fa-sass fa-3x"></i><p>SASS/SCSS</p></li>
+                                <li><i class="fas fa-wind fa-3x"></i><p>Tailwind CSS</p></li>
+                                <li><i class="fab fa-bootstrap fa-3x"></i><p>Bootstrap CSS</p></li>
+                                <li> <i class="fab fa-java fa-3x"></i><p>Java</p></li>
+                                <li><i class="fab fa-python fa-3x"></i><p>Python</p></li>
+                                <li><i class="fab fa-github-square fa-3x"></i><p>Git Version Control</p></li>
+                                <li><i class="fas fa-pen-fancy fa-3x"></i><p>UI UX</p></li>
+                                <li><i class="fas fa-code fa-3x"></i><p>C++</p></li>
+                            </ul>
                             <h2>Learn more about me</h2>
                             <div className = "textbox">
-                                <button className = "tag green-color">Download Resume</button>
+                                <button className = "tag"><i class="fas fa-arrow-down"></i> Download Resume</button>
                             </div>
                         </div>
                     </div>
                     <div className = "skill-card">
                         <div className= "row">
                             <img className = "image" src={me} alt = "" />
-                            <h1>KEVIN SU</h1>
+                            <h1>Kevin Su</h1>
                             <h4>Front-End Developer | Software Engineer</h4>
                             <div className = "summary-box">
                                 <i class="fas fa-graduation-cap fa-3x"></i>
@@ -56,9 +64,9 @@ function About(props) {
                                 <i class="fas fa-envelope fa-2x"></i>
                             </div>
                             <div className = "textbox">
-                                <h1>BACKGROUND</h1>
+                                <h1>Background</h1>
                                 <p>During highschool I took interest in subjects that enable my creativity such as art and graphical design and since then, building aesthetic things has grown to be a passion of mine. </p>
-                                <p>After dabbling in various engineering topics in my first year of university, I have grown to love web development <span className = "green-color">(both front-end and back-end)</span> and is always hungry to learn new things. I like to code things from scratch and bring life to awesome ideas.</p>
+                                <p>After dabbling in various engineering topics in my first year of university, I have grown to love web development (both front-end and back-end) and is always hungry to learn new things. I like to code things from scratch and bring life to awesome ideas.</p>
                             </div>
                         </div>
                         
--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -13,7 +13,7 @@ function Experience(props) {
                         <li>
                             <span></span>
                             <div className = "title">
-                                <h2 className = "green-color">ASSOCIATE FRONT-END DEVELOPER</h2>
+                                <h2>ASSOCIATE FRONT-END DEVELOPER</h2>
                                 <h4>AKQA</h4>
                                 <p>Oct 2021 - Present - Full Time - Sydney, AU</p>
                                 <p>Developed and maintained code for client websites primarily using HTML, CSS, Sass, JavaScript, and jQuery. Performed testing to ensure cross-browser friendliness and mobile responsiveness. Clients includes Bridgestone, Business Australia, Always On Gift Cards, and more...</p>
@@ -23,7 +23,7 @@ function Experience(props) {
                         <li>
                             <span></span>
                             <div className = "title">
-                                <h2 className = "green-color">FRONT-END INTERN</h2>
+                                <h2>FRONT-END INTERN</h2>
                                 <h4>GPOS</h4>
                                 <p>Dec 2019 - Feb 2020 - 3 Months - Auckland, NZ</p>
                                 <p>Developed templates using React and Ant Design. Refactored the website's code from vanilla HTML into a React Application. Wrote extensive integration and unit tests for the templates. Engaged with Agile Methodologies. Improved presentation skills via presenting the product to the team.</p>
@@ -33,7 +33,7 @@ function Experience(props) {
                         <li>
                             <span></span>
                             <div className = "title">
-                                <h2 className = "green-color">QA INTERN</h2>
+                                <h2>QA INTERN</h2>
                                 <h4>GPOS</h4>
                                 <p>Dec 2018 - Feb 2019 - 3 Months - Auckland, NZ</p>
                                 <p>Performed black box and unit testing on a software application using Postman and manually testing the application itself.</p>
@@ -43,7 +43,7 @@ function Experience(props) {
                         <li>
                             <span></span>
                             <div className = "title">
-                                <h2 className = "green-color">CUSTOMER SERVICE</h2>
+                                <h2>CUSTOMER SERVICE</h2>
                                 <h4>Eden Foods</h4>
                                 <p>2015 - 2019 (Part-Time) - Auckland, NZ</p>
                                 <p>Utilized organisational skills to oversee and guide employees. Aided in stock management and control of the daily workflow within the store
@@ -53,6 +53,13 @@ function Experience(props) {
                             <span></span>
                         </li>
                     </ul>
+                </div>
+                <div className = "container">
+                    <div className = "text">
+                        <h1>Career Summary</h1>
+                        <p>I’m currently working at AKQA, a digital design and communications agency owned by WPP based in Sydney, Australia.</p>
+                        <p>I’ve been priviledged to work a variety of jobs throughout my early life - doing everything from Customer Service to Quality Assurance to Front-End Development, as I believe they all contributed into helping me grow into the person I am today. </p>
+                    </div>
                 </div>
             </div>
         </div>
